9fd6b2d62d886ebd47d0a557a4243e06625e0c34
Raghav Sharma <code.practice17@gmail.com>
2026-01-09 18:32:10 +0530

    update

---
+++ process/1_multiple_fork.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+#define SOME_NON_ZERO_NUM 1
+#define COUNT             10
+
+int
+main(void)
+{
+    pid_t id = SOME_NON_ZERO_NUM;
+    for (int i = 0; i < COUNT; i++) {
+        if (id != 0) {
+            id = fork();
+
+            if (id == 0) {
+                printf("Child %d\n", i);
+            }
+        } else {
+            break;
+        }
+    }
+
+}
